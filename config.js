facfcbfd41175eafe3008bc15a4edeec4ebfd61a
zohaibs1011 <63616015+zohaibs1011@users.noreply.github.com>
2023-08-06 07:57:15 +0100

    Initial commit

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://bibiplayz.thetoxicbeastyt.workers.dev",
+  VITE_TMDB_READ_API_KEY: "2c9a975ab43e29311472a07bb5433bdd"
+};
